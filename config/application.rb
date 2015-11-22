f2a3e2654b051086a7eadf2ea3f2b0b89e7c0baa
Safarali <mr.safarali@gmail.com>
2015-11-21 21:27:19 -0800

    test

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,7 @@ module Airlive
     # config.i18n.default_locale = :de
     # Heroku requires this to be false
      config.assets.initialize_on_precompile=false
+     config.serve_static_assets = true
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
