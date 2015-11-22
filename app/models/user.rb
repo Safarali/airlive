2f1c39d2f2689cf18fbd671b209d946a207dc3a7
Safarali <mr.safarali@gmail.com>
2015-11-22 14:54:34 -0800

    hi

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,12 +10,7 @@ class User < ActiveRecord::Base
   
 
 
-  has_attached_file :avatar, dependent: :destroy, :storage => :s3,
-  :s3_credentials => {
-    :bucket => ENV['S3_BUCKET_NAME'],
-    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-  }
+  has_attached_file :avatar, dependent: :destroy
   validates_attachment_content_type :avatar, :content_type => ["image/jpg", "image/jpeg", "image/png"]
   
 
