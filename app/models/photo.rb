8096dbb10bf4cee5e7b229c895600d6d2fe768fc
Safarali <mr.safarali@gmail.com>
2015-11-22 23:51:54 -0800

    test

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -4,7 +4,7 @@ class Photo < ActiveRecord::Base
   has_attached_file :image, 
                           :styles => { medium: "300x300>", thumb: "100x100>" }
                           :storage => :s3,
-                          :bucket => S3_BUCKET_NAME,
-                          :s3_credentials => S3_CREDENTIALS
+                          :bucket => 'S3_BUCKET_NAME',
+                          :s3_credentials => 'S3_CREDENTIALS'
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 end
