1c5f9e7037aa92528c471c9d3353f0c396fe1393
Safarali <mr.safarali@gmail.com>
2017-08-01 20:39:54 -0700

    Fix Transactional emails in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,12 +84,12 @@ Rails.application.configure do
  config.action_mailer.default :charset => "utf-8"
  config.action_mailer.smtp_settings = {
  address: "smtp.gmail.com",
- port: 587,
+ port: '587',
  domain: "airlive.herokuapp.com",
  authentication: "plain",
  enable_starttls_auto: true,
- user_name: ENV["GMAIL_USERNAME"],
- password: ENV["GMAIL_PASSWORD"]
+ user_name: ENV['GMAIL_USERNAME'],
+ password: ENV['GMAIL_PASSWORD']
 }
 
   config.paperclip_defaults = {
