e5dad3ed22044bab7673ab7692c802308b44bb2a
Safarali <mr.safarali@gmail.com>
2017-08-01 21:03:14 -0700

    Fix email sending

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,7 @@ Rails.application.configure do
 
 
 Rails.application.routes.default_url_options[:host] = 'airlive.herokuapp.com'
+config.action_mailer.default_url_options = { :host => 'aurlive.herokuapp.com' }
  config.action_mailer.delivery_method = :smtp
  config.action_mailer.perform_deliveries = true
  config.action_mailer.raise_delivery_errors = false
