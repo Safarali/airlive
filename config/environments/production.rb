cfd3ada4af3a1cbd58db3cffce5fcc2acef05c0b
Safarali <mr.safarali@gmail.com>
2017-08-01 18:50:07 -0700

    Finish email setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,26 +73,24 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-  config.action_mailer.default_url_options = { :host => 'http://airlive.herokuapp.com/' }
-
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
 
   config.action_mailer.default_url_options = { :host => 'airlive.herokuapp.com' }
-   config.action_mailer.delivery_method = :smtp
-   config.action_mailer.perform_deliveries = true
-   config.action_mailer.raise_delivery_errors = false
-   config.action_mailer.default :charset => "utf-8"
-   config.action_mailer.smtp_settings = {
-   address: "smtp.gmail.com",
-   port: 587,
-   domain: "gmail.com",
-   authentication: "plain",
-   enable_starttls_auto: true,
-   user_name: ENV["GMAIL_USERNAME"],
-   password: ENV["GMAIL_PASSWORD"]
- }
+ config.action_mailer.delivery_method = :smtp
+ config.action_mailer.perform_deliveries = true
+ config.action_mailer.raise_delivery_errors = false
+ config.action_mailer.default :charset => "utf-8"
+ config.action_mailer.smtp_settings = {
+ address: "smtp.gmail.com",
+ port: 587,
+ domain: "airlive.herokuapp.com",
+ authentication: "plain",
+ enable_starttls_auto: true,
+ user_name: ENV["GMAIL_USERNAME"],
+ password: ENV["GMAIL_PASSWORD"]
+}
 
   config.paperclip_defaults = {
       :storage => :s3,
