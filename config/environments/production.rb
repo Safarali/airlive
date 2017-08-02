64a9e4b0a9fe33d01c1d0c81587b1fddbc5bbabe
Safarali <mr.safarali@gmail.com>
2017-08-02 01:16:37 -0700

    Fix AWS

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,21 +92,13 @@ config.action_mailer.default_url_options = { :host => 'airlive.herokuapp.com' }
  password: ENV['SENDGRID_PASSWORD']
 }
 
-  # config.paperclip_defaults = {
-  #     :storage => :s3,
-  #     :s3_credentials => {
-  #       :bucket => ENV['S3_BUCKET_NAME'],
-  #       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-  #       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-  #     }
-  #   }
 
   config.paperclip_defaults = {
   storage: :s3,
   s3_credentials: {
-    bucket: ENV.fetch('S3_BUCKET_NAME'),
-    access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
-    secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY')
+    bucket: ENV('S3_BUCKET_NAME'),
+    access_key_id: ENV('AWS_ACCESS_KEY_ID'),
+    secret_access_key: ENV('AWS_SECRET_ACCESS_KEY')
   }
 }
 
